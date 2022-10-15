1955aa0b0418bcb52d5efd568a64f37b783dde39
Miguel Corral Jr <corraljrmiguel@gmail.com>
2022-10-14 21:08:14 -0300

    feat: add spider doctoralia.py - capabable of scraping a single doctor page - yields 7 attributes (missing price)

---
+++ doctoralia/doctoralia/spiders/doctoralia.py
@@ -0,0 +1,47 @@
+import json
+import scrapy
+
+
+class Doctoralia(scrapy.Spider):
+    """Recursively crawls doctoralia.com.br and extracts doctor data."""
+    name = 'DoctoraliaScraper'
+    start_urls = ['https://www.doctoralia.com.br/psicologo']
+
+    def parse(self, response):
+        """Recursively follows links to all Doctoralia doctors and extracts data from them."""
+
+        # follow all the links to each talk on the page calling the parse_doctor callback for each of them
+        doctor_page_links = response.xpath("//span[@data-ga-event='click']")
+        yield from response.follow_all(doctor_page_links, self.parse_doctor)
+
+        # looks for the link to the next page, builds a URL and yields a new request to the next page
+        # pagination_links = response.xpath("//a[@aria-label='next']")
+        # yield from response.follow_all(pagination_links, self.parse)
+
+    
+    def parse_doctor(self, response):
+        """Parses the response, extracting the scraped psychologist data as dicts."""
+
+        # find xpath to the JSON with talk data
+        # raw = response.xpath("//script[@type='application/json']/text()").get()
+        # cast the string 'raw' to a dict
+        # js = json.loads(raw)
+        # create dict with talk data of interest
+        # d = js['props']['pageProps']['videoData']
+        yield {
+            'doctor_id': response.css("script")[6].re_first("DOCTOR_ID:\s(\d+)"),
+            'name': response.css("script").re_first("FULLNAME:\s'(.*?)'").strip(),
+            'location': response.css("script").re_first("NAME:\s'(.*?)'").strip(),  
+            'specialization': response.css("script")[6].re_first("SPECIALIZATION[\s\S]*?NAME:\s'(.*?)'").strip(),
+            'reviews': response.xpath("//div/meta[@itemprop='reviewCount']/@content").get(),
+            # 'price': ,  # choose a specific service, likely 'Consulta Psicologia' or 'Telemedicina'
+            'telemedicine': response.xpath("//script")[8].re_first("virtual\-consultation\-profile'\]\s=\s'(.*?)'"),
+            'url': response.xpath("//script")[8].re_first("\['gtm\-url'\]\s=\s'(.*?)'"),
+            # 'oldest_review_date': ,
+            # 'date_joined': ,
+            # 'education': ,
+            # 'experience_in': ,
+            # 'medical_conditions_treated': ,
+
+        }
+ 
